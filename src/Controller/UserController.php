70937bab0a005fe367bff4de88275e3f79b6807a
root <maxime.aube@protonmail.com>
2021-04-28 11:06:56 +0200

    ajout du bundle reset-password

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,15 +6,36 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class UserController
+ * @package App\Controller
+ * @Route("/user")
+ */
 class UserController extends AbstractController
 {
     /**
-     * @Route("/user", name="user")
+     * @return Response
      */
     public function index(): Response
     {
+        /*
+         * affichages : email, firstname, lastname, phone, resume, photo
+         */
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
+            'page_name' => 'User index'
+        ]);
+    }
+
+    /**
+     * @Route("/edit", name="user_edit")
+     * @return Response
+     */
+    public function edit(): Response
+    {
+        return $this->render('user/edit.html.twig', [
+            'controller_name' => 'UserController',
+            'page_name' => 'User edit'
         ]);
     }
 }
