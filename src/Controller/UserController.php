404d29406a6aad968a94c2d581ce07f18b885ba3
Todd Raharivelo <s.raharivelo@outlook.fr>
2021-04-29 10:30:07 +0200

    added user access + begin template

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,12 +3,14 @@
 namespace App\Controller;
 
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * Class UserController
+ * @IsGranted("ROLE_USER")
  * @package App\Controller
  * @Route("/user")
  */
