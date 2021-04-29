c8cd82a53630be02b361111a17a28ffa0e1a1e20
Nathan Leduc <nathanleducmusique@gmail.com>
2021-04-29 11:25:25 +0200

    - creation entity filiere et formation - ajout de relation entre formation et filiere, et entre formation et user - affichage liste formation (en cours)

---
+++ src/Controller/FormationController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Formation;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FormationController extends AbstractController
+{
+    private EntityManagerInterface $entityManager;
+
+
+    /**
+     * @Route("/formation", name="formation")
+     */
+    public function index(EntityManagerInterface $entityManager): Response
+    {
+        $formations = $entityManager->getRepository(Formation::class)->findAll();
+
+        return $this->render('formation/index.html.twig', [
+            'formations' => $formations,
+        ]);
+    }
+}
